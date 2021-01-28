78919aab181d6a750dc536aaa1bf186c1c0ec075
younhong <younhong@kakao.com>
2021-01-29 01:13:16 +0900

    upload rxjs react hook

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
-import React from 'react';
+// Example for a snoozable alarm clock, lovingly ripped off from:
+// John Lindquist's RxJS with Vue https://www.youtube.com/watch?v=_74d-NdeqOI
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { of, interval, concat, Subject } from 'rxjs';
+import {
+  takeWhile,
+  takeUntil,
+  scan,
+  startWith,
+  repeatWhen,
+  share,
+  filter,
+} from 'rxjs/operators';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const countdown$ = interval(250)
+  .pipe(
+    startWith(60),
+    scan(time => time - 1),
+    takeWhile(time => time > 0)
+  )
+  .pipe(share());
+
+const actions$ = new Subject();
+const snooze$ = actions$.pipe(filter(action => action === 'snooze'));
+const dismiss$ = actions$.pipe(filter(action => action === 'dismiss'));
+
+const snoozeableAlarm$ = concat(countdown$, of('Wake up! 🎉')).pipe(
+  repeatWhen(() => snooze$)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const observable$ = concat(
+  snoozeableAlarm$.pipe(takeUntil(dismiss$)),
+  of('Have a nice day! 🤗')
+);
+
+function App() {
+  const [state, setState] = useState();
+  useEffect(() => {
+    const sub = observable$.subscribe(setState);
+    return () => sub.unsubscribe();
+  }, []);
+
+  return (
+    <>
+      <h3>Alarm Clock</h3>
+      <div className="display">{state}</div>
+      <button className="snooze" onClick={() => actions$.next('snooze')}>
+        Snooze
+      </button>
+      <button className="dismiss" onClick={() => actions$.next('dismiss')}>
+        Dismiss
+      </button>
+    </>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
